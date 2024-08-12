ce7809437e66c6d5167a7ac6c4de8a821db8af9e
João Vitor Petermann <jpetermann@AGEHAB.MS>
2024-08-12 11:58:49 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("switch").addEventListener("click", toggleMode)
+
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de João Vitor, usando casaco branco, barba e fundo com luz apagada"
+    )
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de João Vitor, usando casaco branco, barba e fundo degrade rosa para azul"
+    )
+  }
+}
